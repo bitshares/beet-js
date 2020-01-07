14bced0e44bb56240a3fe41dbbe51e7baf475681
Stefan Schiessl <stefan.schiessl@blockchainprojectsbv.com>
2020-01-07 10:51:31 +0100

    fix bug in localhost fallback connection
    Signed-off-by: Stefan Schiessl <stefan.schiessl@blockchainprojectsbv.com>

--- src/lib/socket.js
+++ src/lib/socket.js
@@ -1,46 +1,76 @@
-let SSL_HOST = 'wss://local.get-beet.io:60556';
-let LOCAL_HOST = 'ws://localhost:60555';
+let SSL_HOST = 'wss://local.get-beet.io:60556/';
+let LOCAL_HOST = 'ws://localhost:60555/';
 let _allowFallback = false;
 
 let getWebSocketConnection = function (onopen = null, onmessage = null, onclose = null, onerror = null) {
-    let host = SSL_HOST;
-    let next = LOCAL_HOST;
+    let _host = SSL_HOST;
+    let _next = LOCAL_HOST;
+    let _ignoreErrorsFrom = [];
 
     let _connect = function (host, next) {
         return new Promise((resolve, reject) => {
-            let socket = new WebSocket(host);
-            socket.onerror = function (event) {
-                // only fallback for an error on first initialisation
-                if (allowFallback && event.timeStamp < 2000 && next !== null) {
-                    event.stopPropagation();
-                    event.preventDefault();
-                    console.log("Falling back to localhost socket", event);
-                    _connect(next, null).then(socket => {
-                        resolve(socket);
-                    }).catch(reject);
-                } else if (onerror != null) {
-                    onerror(event, socket);
-                }
-            };
-            socket.onopen = function (event) {
-                resolve(socket);
-                if (onopen !== null) {
-                    onopen(event, socket);
-                }
-            };
-            socket.onclose = function (event) {
-                if (onclose !== null) {
-                    onclose(event, socket);
-                }
-            };
-            socket.onmessage = function (event) {
-                if (onmessage !== null) {
-                    onmessage(event, socket);
-                }
-            };
+            try {
+                let socket = new WebSocket(host);
+                socket.onerror = function (event) {
+                    // only fallback for an error on first initialisation
+                    if (allowFallback && event.timeStamp < 2000 && next !== null) {
+                        event.stopPropagation();
+                        event.preventDefault();
+                        console.log("Falling back to localhost socket", event);
+                        _ignoreErrorsFrom.push(host);
+                        _connect(next, null).then(socket => {
+                            console.log("error resolve")
+                            resolve(socket);
+                        }).catch(reject);
+                    } else if (onerror != null) {
+                        onerror(event, socket);
+                    }
+                };
+                socket.onopen = function (event) {
+                    if (event.target && event.target.url && _ignoreErrorsFrom.includes(event.target.url)) {
+                        if (socket.readyState === socket.OPEN || socket.readyState === socket.CONNECTING) {
+                            socket.close();
+                        }
+                        console.error("Ignoring onopen for errored socket, this shouldn't happen!");
+                        return;
+                    }
+                    resolve(socket);
+                    // requeue following commands at the end of stack
+                    setTimeout(() => {
+                        if (onopen !== null) {
+                            onopen(event, socket);
+                        }
+                    });
+                };
+                socket.onclose = function (event) {
+                    if (event.target && event.target.url && _ignoreErrorsFrom.includes(event.target.url)) {
+                        console.error("Ignoring onclose for errored socket: ", event.target.url);
+                        return;
+                    }
+                    reject(socket);
+                    if (onclose !== null) {
+                        onclose(event, socket);
+                    }
+                };
+                socket.onmessage = function (event) {
+                    if (event.target && event.target.url && _ignoreErrorsFrom.includes(event.target.url)) {
+                        if (socket.readyState === socket.OPEN || socket.readyState === socket.CONNECTING) {
+                            socket.close();
+                        }
+                        console.error("Ignoring onmessage for errored socket, this shouldn't happen!");
+                        return;
+                    }
+                    if (onmessage !== null) {
+                        onmessage(event, socket);
+                    }
+                };
+            } catch (err) {
+                console.log(err);
+            }
+
         });
     };
-    return _connect(host, next);
+    return _connect(_host, _next);
 };
 
 let allowFallback = function () {
