ebae4cd9452df5758893d0208d07cad3d0aa50fe
Stefan Schiessl <stefan.schiessl@blockchainprojectsbv.com>
2020-01-07 10:51:19 +0100

    remove security issue
    Signed-off-by: Stefan Schiessl <stefan.schiessl@blockchainprojectsbv.com>

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path');
 const webpack = require('webpack');
 
 const config = module.exports = {
+    mode: "production",
     entry: path.resolve('./src/index.js'),
     output: {
         path: path.resolve('./dist'),
