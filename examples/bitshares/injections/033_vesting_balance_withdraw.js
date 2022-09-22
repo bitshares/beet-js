8e986d6da45cce51e682629b5261d007d2c078de
CM <grctestemail@gmail.com>
2022-09-22 17:09:27 +0100

    Update 033_vesting_balance_withdraw.js

--- examples/bitshares/injections/033_vesting_balance_withdraw.js
+++ examples/bitshares/injections/033_vesting_balance_withdraw.js
@@ -2,7 +2,7 @@ import { injection } from '../../lib/injection.js'
 
 let run = async function () { 
   await injection(
-    "InjectExample.034", // script name
+    "InjectExample.033", // script name
     "BTS_TEST", // chain
     "wss://testnet.xbts.io/ws", // wss url
     "vesting_balance_withdraw", // operation name
